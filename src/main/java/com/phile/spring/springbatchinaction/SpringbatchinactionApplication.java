350b981954483b95ea221cab270611eca064c269
philE22 <moonwhlking@gmail.com>
2024-12-28 23:44:43 +0900

    init

---
+++ src/main/java/com/phile/spring/springbatchinaction/SpringbatchinactionApplication.java
@@ -0,0 +1,13 @@
+package com.phile.spring.springbatchinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchinactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchinactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phile/spring/springbatchinaction/SpringbatchinactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phile.spring.springbatchinaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchinactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
